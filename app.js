c09cf9fcb86f419e165e4d5f6bfc714d5d9c61c7
Matt Barker <mattbarker23@hotmail.com>
2024-01-15 16:15:59 +0000

    finish task 3

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const { getTopics } = require("./controllers/topics.controller.js");
+const {
+  getTopics,
+  getEndpoints,
+} = require("./controllers/topics.controller.js");
 const app = express();
 
 app.use(express.json());
 
 app.get("/api/topics", getTopics);
+
+app.get("/api", getEndpoints);
 
 app.all("*", (req, res) => {
   res.status(404).send({ message: "endpoint not found!" });
--- models/topics.model.js
+++ models/topics.model.js
@@ -1,7 +1,16 @@
 const db = require("../db/connection.js");
+const fs = require("fs/promises");
 
 exports.fetchTopics = () => {
   return db.query("SELECT * FROM topics").then(({ rows }) => {
     return rows;
   });
 };
+
+exports.fetchEndpoints = () => {
+  return fs.readFile("endpoints.json", "utf-8").then((data) => {
+    return data;
+  });
+};
+
+// endpoints.json
