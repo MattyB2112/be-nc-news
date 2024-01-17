0ce8f68fd2a54a6b3f6fed3c2099ef4f372ae902
Matt Barker <mattbarker23@hotmail.com>
2024-01-17 11:59:36 +0000

    forgot to merge...

--- models/topics.model.js
+++ models/topics.model.js
@@ -19,30 +19,13 @@ exports.fetchArticleById = (id) => {
 };
 
 exports.fetchArticles = () => {
-  return db.query(`SELECT article_id FROM comments`).then(({ rows }) => {
-    const commentIds = rows;
-    let commentCount = [];
-    commentIds.forEach((comment) => {
-      commentCount.push(comment.article_id);
+  return db
+    .query(
+      `SELECT articles.author, articles.title,articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.article_id) AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id GROUP BY articles.article_id ORDER BY articles.created_at ASC`
+    )
+    .then(({ rows }) => {
+      return rows;
     });
-    return db
-      .query(
-        `SELECT author, title, article_id, topic, created_at, votes, article_img_url FROM articles ORDER BY created_at DESC`
-      )
-      .then(({ rows }) => {
-        rows.forEach((row) => {
-          row.comment_count = 0;
-        });
-        rows.forEach((row) => {
-          for (let i = 0; i < commentCount.length; i++) {
-            if (row.article_id === commentCount[i]) {
-              row.comment_count++;
-            }
-          }
-        });
-        return rows;
-      });
-  });
 };
 
 exports.fetchArticleComments = (id) => {
@@ -54,6 +37,7 @@ exports.fetchArticleComments = (id) => {
       if (rows.length === 0) {
         return Promise.reject({ status: 404, message: "not found" });
       }
+
       return rows;
     });
 };
