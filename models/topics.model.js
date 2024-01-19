0e25f68670fd37c1e6c8fda2ed04b8ba4cad1b39
Matt Barker <mattbarker23@hotmail.com>
2024-01-19 15:15:06 +0000

    finished code and testing

--- models/topics.model.js
+++ models/topics.model.js
@@ -21,31 +21,47 @@ exports.fetchArticleById = (id) => {
     });
 };
 
-exports.fetchArticles = (query) => {
-  if (query) {
+exports.fetchArticles = (sort_by = "created_at", order = "ASC", topic) => {
+  const validOrderQueries = ["ASC", "DESC"];
+  const validSortByQueries = [
+    "author",
+    "title",
+    "article_id",
+    "topic",
+    "created_at",
+    "votes",
+    "article_img_url",
+    "comment_count",
+  ];
+  if (
+    !validOrderQueries.includes(order) ||
+    !validSortByQueries.includes(sort_by)
+  ) {
+    return Promise.reject({ status: 400, message: "Invalid query" });
+  }
+  let query = `SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.article_id) AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id`;
+
+  if (topic) {
     return db
-      .query(`SELECT * FROM topics WHERE slug = $1`, [query])
+      .query(`SELECT * FROM topics WHERE slug = $1`, [topic])
       .then(({ rows }) => {
         if (rows.length === 0) {
           return Promise.reject({ status: 404, message: "not found" });
         } else
-          return db
-            .query(
-              `SELECT articles.author, articles.title,articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.article_id) AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id WHERE articles.topic = $1 GROUP BY articles.article_id ORDER BY articles.created_at ASC`,
-              [query]
-            )
-            .then(({ rows }) => {
-              return rows;
-            });
+          query += ` WHERE articles.topic = $1 GROUP BY articles.article_id ORDER BY articles.${sort_by} ${order}`;
+        return db.query(query, [topic]).then(({ rows }) => {
+          return rows;
+        });
       });
   } else
-    return db
-      .query(
-        `SELECT articles.author, articles.title,articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.article_id) AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id GROUP BY articles.article_id ORDER BY articles.created_at ASC`
-      )
-      .then(({ rows }) => {
-        return rows;
-      });
+    query += ` GROUP BY articles.article_id ORDER BY articles.${sort_by} ${order}`;
+  return db
+    .query(
+      `SELECT articles.author, articles.title,articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.article_id) AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id GROUP BY articles.article_id ORDER BY articles.${sort_by} ${order}`
+    )
+    .then(({ rows }) => {
+      return rows;
+    });
 };
 
 exports.fetchArticleComments = (id) => {
