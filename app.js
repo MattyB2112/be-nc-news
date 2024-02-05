e9d04ee8de9cb5f0bd6358414692f53e2d8e3f26
Matt Barker <mattbarker23@hotmail.com>
2024-02-05 11:06:41 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,36 +1,24 @@
+const cors = require("cors");
 const express = require("express");
 const {
-  getTopics,
-  getEndpoints,
-  getArticleById,
-  getArticles,
-  getArticleComments,
   postArticleComment,
   changeProperty,
   deleteComment,
-  getUsers,
 } = require("./controllers/topics.controller.js");
 const app = express();
+const apiRouter = require("./api-router.js");
+
+app.use(cors());
 
 app.use(express.json());
 
-app.get("/api/topics", getTopics);
-
-app.get("/api", getEndpoints);
-
-app.get("/api/articles/:article_id", getArticleById);
-
-app.get("/api/articles", getArticles);
-
-app.get("/api/articles/:article_id/comments", getArticleComments);
+app.use("/api", apiRouter);
 
 app.post("/api/articles/:article_id/comments", postArticleComment);
 
 app.patch("/api/articles/:article_id", changeProperty);
 
 app.delete("/api/comments/:comment_id", deleteComment);
-
-app.get("/api/users", getUsers);
 
 app.use((err, req, res, next) => {
   if (err.status && err.message) {
